7ea0231acdf285f42324365d4d0a58068de5418b
Meticulus <theonejohnnyd@gmail.com>
2017-02-14 06:36:28 -0600

    touchscreen: Implement my own DT2W
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- drivers/input/misc/hisi_powerkey.c
+++ drivers/input/misc/hisi_powerkey.c
@@ -34,6 +34,9 @@
 #include <asm/irq.h>
 
 #include <linux/hisi/util.h>
+
+#include <linux/dt2w.h>
+
 #ifdef CONFIG_HISI_HI6XXX_PMIC
 #include <soc_smart_interface.h>
 #endif
@@ -302,6 +305,7 @@ static int hisi_powerkey_probe(struct platform_device *pdev)
 		ret = -ENOENT;
 		goto input_err;
 	}
+	register_power_input(info->idev);
 
 	platform_set_drvdata(pdev, info);
 
